ef8dbfd1b3d13df6b509a69662bd140801d98bae
kis89576@naver.com <kis89576@naver.com>
2022-02-04 18:06:59 +0900

    Change Setting to Gradle

--- remind_server/src/main/java/com/team5/remind_server/RemindServerApplication.java
+++ remind_server/src/main/java/com/team5/remind_server/RemindServerApplication.java
@@ -1,4 +1,4 @@
-package com.team5.remind_backend;
+package com.team5.remind_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource(value = { "classpath:${jdbc.config}"})
-public class RemindBackendApplication {
+public class RemindServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RemindBackendApplication.class, args);
+        SpringApplication.run(RemindServerApplication.class, args);
     }
 
 }
